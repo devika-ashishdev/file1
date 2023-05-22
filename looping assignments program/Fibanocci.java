f7131b87a0a94a0eb86ffa1590a0d3af9885f7f9
devika-ashishdev <132260978+devika-ashishdev@users.noreply.github.com>
2023-05-22 12:59:00 +0400

    Add files via upload

---
+++ looping assignments program/Fibanocci.java
@@ -0,0 +1,15 @@
+class Fibanocci {
+public static void main(String args[])  
+{    
+ int f1=0,f2=1,f3<=20;    
+ System.out.print(f1+" "+f2);    
+    
+ while    
+ {    
+  f3=f1+f2;    
+  System.out.print(" "+f3);    
+  f1=f2;    
+  f2=f3;    
+ }    
+  
+}}  
