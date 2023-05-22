e3c2fdc9474475c6072b36d56f39418c4d581e73
devika-ashishdev <132260978+devika-ashishdev@users.noreply.github.com>
2023-05-22 12:55:35 +0400

    Add files via upload
    added files

---
+++ DevikaAthiyarath.java
@@ -0,0 +1,7 @@
+class DevikaAthiyarath
+{
+public static void main(String arg[])
+{
+System.out.println("hi how are you devika");
+}
+}
---
+++ obsqura.java
@@ -0,0 +1,6 @@
+class obsqura {
+public static void main(String arg[])
+{
+System.out.println("welocme to new batch");
+}
+}
